f8d1183039ca14a7eef25eea9d5fe0f668ab3125
Piyush Gupta <piyushg259@gmail.com>
2025-07-08 01:40:13 -0700

    download csv done

--- application/config.py
+++ application/config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Config():
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -12,19 +11,4 @@ class LocalDevelopmentConfig(Config):
     SECURITY_PASSWORD_HASH = 'bcrypt'
     SECURITY_PASSWORD_SALT = 'this-is-my-salt'
     WTF_CSRF_ENABLED = False
-=======
-class Config():
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-class LocalDevelopmentConfig(Config):
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-    DEBUG = True
-
-    SECRET_KEY = 'this-is-my-secret-key'
-    SECURITY_PASSWORD_HASH = 'bcrypt'
-    SECURITY_PASSWORD_SALT = 'this-is-my-salt'
-    WTF_CSRF_ENABLED = False
->>>>>>> cac058d (export backend done)
     SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
--- application/database.py
+++ application/database.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-=======
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
->>>>>>> cac058d (export backend done)
